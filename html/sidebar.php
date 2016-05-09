8d2520c32fbe7268c94802ed6f3db51d212a2fca
Cinthia Fukami <cinthiafukami@gmail.com>
2016-05-09 14:18:49 -0300

    adicionado submenu da barra lateral

--- html/sidebar.php
+++ html/sidebar.php
@@ -2,8 +2,23 @@
   <div class="col-sm-2 menu">
     <ul class="menu-principal">
       <a href="/pellicanos/html/index.php"><li><span class="glyphicon glyphicon-home" aria-hidden="true"></span> &nbsp;Home</li></a>
-      <a href="/pellicanos/html/listar-conta-pagar.php"><li><span class="glyphicon glyphicon-transfer" aria-hidden="true"></span> &nbsp;Contas a pagar</li></a>
-      <a href="/pellicanos/html/listar-fornecedores.php"><li><span class="glyphicon glyphicon-user" aria-hidden="true"></span> &nbsp;Fornecedores</li></a>
-      <a href="/pellicanos/html/listar-contas-bancarias.php"><li><span class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></span> &nbsp;Contas bancárias</li></a>
+      <a href="/pellicanos/html/listar-conta-pagar.php"><li><span class="glyphicon glyphicon-transfer" aria-hidden="true"></span> &nbsp;Contas a pagar
+      <ul>
+      	<a href="/pellicanos/html/listar-conta-pagar.php"><li>Todas as contas a pagar</li></a>
+      	<a href="/pellicanos/html/cadastro-conta-pagar.php"><li>Adicionar nova</li></a>
+      </ul></li></a>
+      <a href="/pellicanos/html/listar-fornecedores.php"><li><span class="glyphicon glyphicon-user" aria-hidden="true"></span> &nbsp;Fornecedores
+      <ul>
+      	<a href="/pellicanos/html/listar-fornecedores.php"><li>Todos os fornecedores</li></a>
+      	<a href="/pellicanos/html/cadastro-fornecedor.php"><li>Adicionar novo</li></a>
+      </ul></li></a>
+      <a href="/pellicanos/html/listar-contas-bancarias.php"><li><span class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></span> &nbsp;Contas bancárias 
+      <ul>
+      	<a href="/pellicanos/html/listar-contas-bancarias.php"><li>Todas as contas bancárias</li></a>
+      	<a href="/pellicanos/html/cadastro-conta-bancaria.php"><li>Adicionar nova</li></a>
+      	<a href=""><li>Bancos</li></a>
+      	<a href=""><li>Agências bancárias</li></a>
+      	<a href=""><li>Centro de custos</li></a>
+      </ul></li></a>
     </ul>
   </div>
