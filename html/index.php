46d5b81047e04c1e3ed1252e50b54054596e24cf
Cinthia Fukami <cinthiafukami@gmail.com>
2016-05-09 11:18:18 -0300

    criado pagina index

--- html/index.php
+++ html/index.php
@@ -1,1 +1,10 @@
-index.php
+<?php include "header.php" ?>
+<?php include "sidebar.php" ?>
+
+      <div class="col-sm-10">
+        <div class="conteudo">
+          <h1>Bem vindo, Administrador!</h1>
+      </div>
+    </div><br><br>
+  
+<?php include "footer.php" ?>
