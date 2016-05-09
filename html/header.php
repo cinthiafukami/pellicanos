bbc12d1ca526254818941342cdc908cc2e048e81
Cinthia Fukami <cinthiafukami@gmail.com>
2016-05-09 15:57:30 -0300

    criado submenu na barra superior de usuarios

--- html/header.php
+++ html/header.php
@@ -17,7 +17,12 @@
     <header>
       <div class="logo"><img src="img/logo.png" alt=""></div>
       <div class="header-menu">
-        <div class="alertas"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> Alertas</div>
-        <div class="user"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> &nbsp;Administrador &nbsp;<span class="glyphicon glyphicon-menu-down" style="color: #999; font-size: 10px" aria-hidden="true"></span></div>
+        <div class="alertas"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> Alertas &nbsp;<span class="glyphicon glyphicon-menu-down" style="color: #999; font-size: 10px" aria-hidden="true"></span></div>
+        <div class="user"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> &nbsp;Administrador &nbsp;<span class="glyphicon glyphicon-menu-down" style="color: #999; font-size: 10px" aria-hidden="true"></span>
+        <ul>
+          <li>Ver todos os usuários</li>
+          <li>Criar novo usuário</li>
+          <a href="/pellicanos/html/login.php"><li>Logout</li></a>
+        </ul></div>
       </div>
     </header>
