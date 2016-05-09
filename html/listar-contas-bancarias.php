9810b5b0df7b4cbc7353b9bb892ecd383f2d02b5
Cinthia Fukami <cinthiafukami@gmail.com>
2016-05-09 11:20:33 -0300

    criado pagina de listar contas bancarias

---
+++ html/listar-contas-bancarias.php
@@ -0,0 +1,8 @@
+<?php include "header.php" ?>
+<?php include "sidebar.php" ?>
+  
+<div class="col-sm-10">
+  
+</div>
+
+<?php include "footer.php" ?>
--- html/sidebar.php
+++ html/sidebar.php
@@ -1,9 +1,9 @@
 <div class="row">
   <div class="col-sm-2 menu">
     <ul class="menu-principal">
-      <a href="/"><li><span class="glyphicon glyphicon-home" aria-hidden="true"></span> &nbsp;Home</li></a>
+      <a href="/pellicanos/html/index.php"><li><span class="glyphicon glyphicon-home" aria-hidden="true"></span> &nbsp;Home</li></a>
       <a href="/pellicanos/html/listar-conta-pagar.php"><li><span class="glyphicon glyphicon-transfer" aria-hidden="true"></span> &nbsp;Contas a pagar</li></a>
       <a href="/pellicanos/html/listar-fornecedores.php"><li><span class="glyphicon glyphicon-user" aria-hidden="true"></span> &nbsp;Fornecedores</li></a>
-      <a href=""><li><span class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></span> &nbsp;Contas bancárias</li></a>
+      <a href="/pellicanos/html/listar-contas-bancarias.php"><li><span class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></span> &nbsp;Contas bancárias</li></a>
     </ul>
   </div>
